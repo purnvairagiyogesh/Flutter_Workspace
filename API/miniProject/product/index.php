2308fb1bfea83fb474fa2f1737c5c43ab48f5e22
purnvairagiyogesh <purnvairagiyogesh@gmail.com>
2026-08-12 19:22:02 -0700

    Add files via upload

---
+++ API/miniProject/product/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo "<h2>Yogesh API</h2>";
+    echo "<ul>";
+    echo "<li><a href='yogesh/product_insert.php'>Insert Data</a></li>";
+    echo "<li><a href='yogesh/product_view.php'>View Data with Ordering</a></li>";
+    echo "<li><a href='yogesh/product_view&insert.php'>View and Insert final price</a></li>";
+    echo "<li><a href='yogesh/product_update.php'>Update Data</a></li>";
+    echo "<li><a href='yogesh/delete_product.php'>Delete Data</a></li>";
+
+    echo "</ul>";
+
+?>
