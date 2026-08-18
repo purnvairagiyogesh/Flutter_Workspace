081e6268fc4fb5efcf1d689f7e2f43e87c70189b
purnvairagiyogesh <purnvairagiyogesh@gmail.com>
2026-08-18 06:53:36 +0530

    Create admin_login.php

---
+++ API/modified_mini_project _api/admin_login.php
@@ -0,0 +1,24 @@
+<?php
+
+    include('connection.php');
+
+    $email = $_REQUEST['email'];
+    $pass = $_REQUEST['password'];
+
+    $sql = "select * from mini_users where email = '$email' and password = '$pass' and identifier = 'Admin'";
+    $result = mysqli_query($con, $sql);
+
+    $num = mysqli_num_rows($result);
+
+    if($num > 0)
+    {
+        $fetch = mysqli_fetch_array($result);
+        echo json_encode(['code' => 200]);
+    }    
+    else 
+    {
+        echo "$email". " ". "$pass";
+        echo "0";
+    }    
+
+?>
