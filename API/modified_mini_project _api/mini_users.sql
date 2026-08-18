a4977e9f9df9804d6f6b657ad7eed53b45eb0c6e
purnvairagiyogesh <purnvairagiyogesh@gmail.com>
2026-08-17 22:17:55 -0700

    Add files via upload

---
+++ API/modified_mini_project _api/mini_users.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 18, 2026 at 07:17 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `moblie_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mini_users`
+--
+
+CREATE TABLE `mini_users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(12) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `identifier` varchar(50) NOT NULL,
+  `user_photo` text NOT NULL,
+  `user_adharcard` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mini_users`
+--
+
+INSERT INTO `mini_users` (`id`, `name`, `surname`, `gender`, `email`, `phone`, `password`, `identifier`, `user_photo`, `user_adharcard`) VALUES
+(1, 'yogesh', 'purnvairagi', 'male', 'yogesh@gmail.com', '9106493491', '1234', 'Admin', '', ''),
+(4, 'yogesh', 'p', 'male', 'yogi@gmail.com', '1231123123', '1234', 'user', 'https://localhost/Mini Projct API/images/user_6901.jpg', 'https://localhost/Mini Projct API/images/adhar_1208.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mini_users`
+--
+ALTER TABLE `mini_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mini_users`
+--
+ALTER TABLE `mini_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
